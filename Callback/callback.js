aa3af5ed363af8478ab313e8031d69b78fdf4afe
Suhas Gondukupi <suhas8838@gmail.com>
2025-03-10 11:15:48 +0530

    callback

--- Callback/callback.js
+++ Callback/callback.js
@@ -1,13 +1,26 @@
+// Callback : It is a function that is passed as argumment to another function
+
+
+
+//way 1 : create separate function then pass it as callback
 function sum(a,b){
     console.log(a+b)
 }
-
-
 
 function calculator(a,b,sumCallback){
     sumCallback(a,b)
 }
 
-
-
 calculator(34,10,sum)   //44
+
+
+
+//way 2 : directly write function while calling function
+
+function subtract(a,b,subCallback){
+    subCallback(a,b)
+}
+
+subtract(30,20,(a,b)=>{
+    console.log(a-b)
+})
--- Callback/setTimeout.js
+++ Callback/setTimeout.js
@@ -1,8 +1,29 @@
+
+
+const greet = ()=>{
+    
+}
+
+
+
+
+
 setTimeout(()=>{
     console.log('After 3s')
 },3000)
 
 
+
+
+
+
+
+
+
+
 // setInterval(()=>{
 //     console.log('after each 2 s')
 // },2000)
+
+
+
